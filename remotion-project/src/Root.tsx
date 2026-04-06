57e4a5ab46f71a5b91b9ceb2d03ef153defa3e17
M07amedm0usa <155415556+M07amedm0usa@users.noreply.github.com>
2026-04-06 21:49:26 +0200

    Remove Arabic comment about duration in Root.tsx
    Removed comment in Arabic about default duration for Remotion.

--- remotion-project/src/Root.tsx
+++ remotion-project/src/Root.tsx
@@ -2,13 +2,12 @@ import { Composition } from "remotion";
 import { MyVideo } from "./Composition";
 
 export const RemotionRoot: React.FC = () => {
-  // هنا بنقول لريموشن: لو مبعتلكش مدة، افترض إنها 300 فريم (10 ثواني) مبدئياً
   return (
     <>
       <Composition
         id="MyComp"
         component={MyVideo}
-        durationInFrames={450} // خليها رقم كبير كفاية (15 ثانية مثلاً)
+        durationInFrames={450} 
         fps={30}
         width={1080}
         height={1920}

b423d0610105dfb7e2caa64f3473a733fcdd7296
M07amedm0usa <155415556+M07amedm0usa@users.noreply.github.com>
2026-04-06 21:33:05 +0200

    Update Root.tsx

--- remotion-project/src/Root.tsx
+++ remotion-project/src/Root.tsx
@@ -1,16 +1,21 @@
 import { Composition } from "remotion";
-import { MyComposition } from "./Composition";
+import { MyVideo } from "./Composition";
+import "./style.css";
 
 export const RemotionRoot: React.FC = () => {
+  // هنا بنقول لريموشن: لو مبعتلكش مدة، افترض إنها 300 فريم (10 ثواني) مبدئياً
   return (
     <>
       <Composition
         id="MyComp"
-        component={MyComposition}
-        durationInFrames={60}
+        component={MyVideo}
+        durationInFrames={450} // خليها رقم كبير كفاية (15 ثانية مثلاً)
         fps={30}
-        width={1280}
-        height={720}
+        width={1080}
+        height={1920}
+        defaultProps={{
+            stepsJsonPath: "/steps.json"
+        }}
       />
     </>
   );
